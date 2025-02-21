3f4f6e68053aa02c781b77d02c1675b8e57b8ef0
Elham <elham.heydari722@gmail.com>
2025-02-21 14:07:42 +0330

    add dependency container

---
+++ WebStore.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using WebStore.Application.Interfaces;
+using WebStore.Application.Services;
+using WebStore.Domain.Intefaces;
+using WebStore.Infra.Data.Repositories;
+
+namespace WebStore.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Application Layer
+            service.AddScoped<IProductService, ProductService>();
+
+            // Infra Data Layer
+
+            service.AddScoped<IProductRepository,ProductRepository>();
+        }
+    }
+}
