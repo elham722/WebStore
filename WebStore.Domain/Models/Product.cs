6ca373bebb5ad09baf79d3dcb7d25a3586e3dc85
Elham <elham.heydari722@gmail.com>
2025-02-21 16:08:11 +0330

    change imgurl to nullable for products

--- WebStore.Domain/Models/Product.cs
+++ WebStore.Domain/Models/Product.cs
@@ -11,6 +11,6 @@ namespace WebStore.Domain.Models
         public int Id  { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
