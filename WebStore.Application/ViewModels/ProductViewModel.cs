1723a08f9f8fddf1477a7dd9a43b50cbd9c538c4
Elham <elham.heydari722@gmail.com>
2025-02-21 12:06:36 +0330

    add interfaces and service and repo

---
+++ WebStore.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+
+namespace WebStore.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ WebStore.Domain/Intefaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+
+namespace WebStore.Domain.Intefaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
