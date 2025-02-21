34fc1646284406ce9bd427540e9e20d024f9019c
Elham <elham.heydari722@gmail.com>
2025-02-21 16:30:28 +0330

    change ferrences

---
+++ WebStore/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Application.Services.Interfaces;
+
+namespace WebStore.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
