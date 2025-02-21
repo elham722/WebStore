cd66b7db70d90dfa36d8b0ce6e3125c2bc70da80
Elham <elham.heydari722@gmail.com>
2025-02-21 10:55:22 +0330

    add data layer and context

---
+++ WebStore.Infra.Data/Context/WebStoreDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Models;
+
+namespace WebStore.Infra.Data.Context
+{
+  public class WebStoreDBContext:DbContext
+    {
+        public WebStoreDBContext(DbContextOptions<WebStoreDBContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
